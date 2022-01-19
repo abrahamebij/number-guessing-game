a82d829b4b339e591778b162263a4dc8b9067974
Abraham Ebijuni <91433818+Ademola1235@users.noreply.github.com>
2022-01-19 07:14:58 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,32 +1,3 @@
-var cacheName = 'Number Guessing Game';
-var filesToCache = [
-  '/',
-  'index.html',
-  'assets/styles.css',
-  'assets/index.js',
-  'assets/pic.png',
-  'assets/fc.jpg',
-  'assets/1_pic.png',
-  'assets/2_pic.png',
-  'assets/3_pic.png',
-  'assets/4_pic.png'
-];
-
-/* Start the service worker and cache all of the app's content */
-self.addEventListener('install', function(e) {
-  e.waitUntil(
-    caches.open(cacheName).then(function(cache) {
-      return cache.addAll(filesToCache);
-    })
-  );
-  self.skipWaiting();
-});
-
-/* Serve cached content when offline */
-self.addEventListener('fetch', function(e) {
-  e.respondWith(
-    caches.match(e.request).then(function(response) {
-      return response || fetch(e.request);
-    })
-  );
+self.addEventListener("fetch", function(event) {
+  console.log('Service Worker is up and running');
 });
